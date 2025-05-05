8bbc11db52ab613e65fc8e8413d7b86285e2ba96
hooniegit <neivekim76@gmail.com>
2025-05-05 19:37:16 +0900

    250505 feat - Spring Project Base

---
+++ src/test/java/com/hooniegit/SpringDigger/SpringDiggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hooniegit.SpringDigger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDiggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
